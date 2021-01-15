204b995068d60fa506aca9550d4d9b74dc5816d1
Jubair Ahmed Junjun <jubair.ahmed.junjun@gmail.com>
2021-01-15 19:45:03 +0600

    data type converstion and taking value from user

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -6,8 +6,54 @@ namespace HelloWorldApp
     {
         static void Main(string[] args)
         {
+            //Taking input from user
+
+            Console.Write("Please give your name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Output is : " +name);
+
+            Console.Write("Enter Your first Number : ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please Enter Your second Number : ");
+            int secondNumber =Convert.ToInt32(Console.ReadLine());
+            int add = firstNumber + secondNumber;
+            int sub = firstNumber - secondNumber;
+            int mul = firstNumber * secondNumber;
+            int div = firstNumber / secondNumber;
+            int rem = firstNumber % secondNumber;
+
+            Console.WriteLine("Result : " +add);
+            Console.WriteLine("Result : " + sub);
+            Console.WriteLine("Result : " + mul);
+            Console.WriteLine("Result : " + div);
+            Console.WriteLine("Result : " + rem);
+
+            Console.ReadKey();
+            
+            
+            
+            
+            
+            //variable
+
+
+        /*  string name = "Jubair Ahmed Junjun";
+            int age = 20;
+            double phone = 08549xxx;
+            bool b = true;
+            Console.WriteLine(name + "\n" + age + "\n" + phone +"\n"+ b);
+            Console.ReadLine();
+
+         */
+            
+            
+            //Hello world print
+        
+            
+         /*
             Console.WriteLine("Hello World!");
             Console.ReadLine();
+         */
         }
     }
 }
